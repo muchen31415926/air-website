16439a6d0b5a0f6e38c4cc522e2e4c9242a07fd1
mu <muchen31415926@gmail.com>
2026-07-31 00:46:57 +0800

    Enable tz_aware for MongoDB client

--- db_wrapper.py
+++ db_wrapper.py
@@ -1,4 +1,5 @@
 import os
+from zoneinfo import ZoneInfo
 
 from dotenv import load_dotenv
 from pymongo import MongoClient
@@ -6,11 +7,12 @@ from pymongo import MongoClient
 load_dotenv()
 
 uri = os.getenv("MONGODB_URI")
+TAIPEI_TZ = ZoneInfo("Asia/Taipei")
 
 
 class DBWrapper:
     def __init__(self):
-        self.client = MongoClient(uri)
+        self.client = MongoClient(uri, tz_aware=True)
         self.db = self.client["air"]
         self.second_collection = self.db["second_air_data"]
         self.minute_collection = self.db["minute_air_data"]
@@ -22,6 +24,13 @@ class DBWrapper:
             collection = self.minute_collection
 
         results = list(collection.find({}).sort("timestamp", -1).limit(limit))
+
         results.reverse()
+        self._convert_to_taipei_time(results)
 
         return results
+
+    @staticmethod
+    def _convert_to_taipei_time(docs):
+        for doc in docs:
+            doc["timestamp"] = doc["timestamp"].astimezone(TAIPEI_TZ)
--- sensor.py
+++ sensor.py
@@ -1,8 +1,9 @@
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
 
 from db_wrapper import DBWrapper
 
-TZ = timezone(timedelta(hours=8))
+TAIPEI_TZ = ZoneInfo("Asia/Taipei")
 
 
 class SensorManager:
@@ -11,7 +12,7 @@ class SensorManager:
 
     def is_online(self):
 
-        now = datetime.now(TZ)
+        now = datetime.now(TAIPEI_TZ)
         ts = self.db.find_data("second", limit=1)[0]["timestamp"]
 
         return now - ts < timedelta(minutes=5)
